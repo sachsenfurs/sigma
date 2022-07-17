256694d74c486495926aa126e0a42ec262176b05
kidran <mail@kidran.de>
2022-07-17 02:15:05 +0200

    Update show.blade.php

--- resources/views/locations/show.blade.php
+++ resources/views/locations/show.blade.php
@@ -7,32 +7,35 @@
                 {{ $location->name }}
             </div>
             <div class="card-body">
-                <div class="row">
+
                     @forelse($location->sigEvents AS $sigEvent)
-                        {{--
-                        // FIXME
-                        Events, die ne abweichende Location haben, werden hier nicht angezeigt
-                        --}}
-                        <div class="col-3">
-                            <strong>
-                                <a href="{{ route("sigs.edit", $sigEvent) }}">{{ $sigEvent->name }}</a>
-                            </strong>
+                        <div class="row mb-3">
+                            {{--
+                            // FIXME
+                            Events, die ne abweichende Location haben, werden hier nicht angezeigt
+                            --}}
+                            <div class="col-3">
+                                <strong>
+                                    <a href="{{ route("sigs.edit", $sigEvent) }}">{{ $sigEvent->name }}</a>
+                                </strong>
+                            </div>
+                            <div class="col-2">
+                                @foreach($sigEvent->timetableEntries AS $entry)
+                                    <p>
+                                        <b>{{ $entry->start->format("d.m.Y") }}</b><br>
+                                        {{ $entry->start->format("H:i") }} - {{ $entry->end->format("H:i") }}
+                                    </p>
+                                @endforeach
+                            </div>
+                            <div class="col-7">
+                                <td>{{ $sigEvent->description }}</td>
+                            </div>
                         </div>
-                        <div class="col-2">
-                            @foreach($sigEvent->timetableEntries AS $entry)
-                                <p>
-                                    <b>{{ $entry->start->format("d.m.Y") }}</b><br>
-                                    {{ $entry->start->format("H:i") }} - {{ $entry->end->format("H:i") }}
-                                </p>
-                            @endforeach
-                        </div>
-                        <div class="col-7">
-                            <td>{{ $sigEvent->description }}</td>
-                        </div>
+                        {!! $loop->remaining > 0 ? "<hr>" : "" !!}
                     @empty
                         Keine SIGs zugeordnet
                     @endforelse
-                </div>
+
             </div>
         </div>
     </div>
