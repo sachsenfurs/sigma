2a70954dfa163a14cc390a100cf1f8d01b7bcb57
Kenthanar <d.ilday@kenthart.de>
2023-02-08 16:03:47 +0100

    Update Routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Auth\LoginController;
+use \App\Http\Controllers\Auth\OAuthLoginController;
+use \App\Http\Controllers\Public\TableViewController;
+use \App\Http\Controllers\Public\TimeslotShowController;
+use \App\Http\Controllers\HomeController;
+use \App\Http\Controllers\User\UserController;
+use \App\Http\Controllers\Sig\SigEventController;
+use \App\Http\Controllers\Sig\SigHostController;
+use \App\Http\Controllers\Sig\SigLocationController;
+use \App\Http\Controllers\TimetableController;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,45 +29,45 @@ use Illuminate\Support\Facades\Route;
 
 
 // Auth
-Route::get("/login", [\App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name("login");
-Route::post("/login", [\App\Http\Controllers\Auth\LoginController::class, 'login']);
-Route::post("/logout", [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name("logout");
+Route::get("/login", [LoginController::class, 'showLoginForm'])->name("login");
+Route::post("/login", [LoginController::class, 'login']);
+Route::post("/logout", [LoginController::class, 'logout'])->name("logout");
 
-Route::get("/oauthlogin", [\App\Http\Controllers\Auth\OAuthLoginController::class, 'index'])->name("oauthlogin");
-Route::get("/oauth", [\App\Http\Controllers\Auth\OAuthLoginController::class, 'redirect']);
+Route::get("/oauthlogin", [OAuthLoginController::class, 'index'])->name("oauthlogin");
+Route::get("/oauth", [OAuthLoginController::class, 'redirect']);
 
-Route::get("/table", [\App\Http\Controllers\Public\TableViewController::class, 'index'])->name("public.tableview");
-Route::get("/show/{entry}", [\App\Http\Controllers\Public\TimeslotShowController::class, 'index'])->name("public.timeslot-show");
+Route::get("/table", [TableViewController::class, 'index'])->name("public.tableview");
+Route::get("/show/{entry}", [TimeslotShowController::class, 'index'])->name("public.timeslot-show");
 Route::group(['middleware' => "auth"], function() {
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::get('/users', [\App\Http\Controllers\User\UserController::class, 'index'])->name("users.index");
-    Route::post('/users', [\App\Http\Controllers\User\UserController::class, 'store'])->name("users.store");
-    Route::delete('/users/{user}', [\App\Http\Controllers\User\UserController::class, 'destroy'])->name("users.destroy");
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/users', [UserController::class, 'index'])->name("users.index");
+    Route::post('/users', [UserController::class, 'store'])->name("users.store");
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name("users.destroy");
 
     // SIG Events
-    Route::get("/sigs", [\App\Http\Controllers\Sig\SigEventController::class, 'index'])->name("sigs.index");
-    Route::get("/sigs/create", [\App\Http\Controllers\Sig\SigEventController::class, 'create'])->name("sigs.create");
-    Route::post("/sigs", [\App\Http\Controllers\Sig\SigEventController::class, 'store'])->name("sigs.store");
-    Route::get("/sigs/{sig}/edit", [\App\Http\Controllers\Sig\SigEventController::class, 'show'])->name("sigs.edit");
-    Route::put("/sigs/{sig}", [\App\Http\Controllers\Sig\SigEventController::class, 'update'])->name("sigs.update");
-    Route::delete("/sigs/{sig}", [\App\Http\Controllers\Sig\SigEventController::class, 'destroy'])->name("sigs.destroy");
+    Route::get("/sigs", [SigEventController::class, 'index'])->name("sigs.index");
+    Route::get("/sigs/create", [SigEventController::class, 'create'])->name("sigs.create");
+    Route::post("/sigs", [SigEventController::class, 'store'])->name("sigs.store");
+    Route::get("/sigs/{sig}/edit", [SigEventController::class, 'show'])->name("sigs.edit");
+    Route::put("/sigs/{sig}", [SigEventController::class, 'update'])->name("sigs.update");
+    Route::delete("/sigs/{sig}", [SigEventController::class, 'destroy'])->name("sigs.destroy");
 
     // SIG Hosts
-//    Route::get("/hosts", [\App\Http\Controllers\Sig\SigHostController::class, 'index'])->name("hosts.index");
-//    Route::get("/hosts/{host}", [\App\Http\Controllers\Sig\SigHostController::class, 'show'])->name("hosts.show");
-//    Route::get("/hosts/{host}/edit", [\App\Http\Controllers\Sig\SigHostController::class, 'edit'])->name("hosts.edit");
-//    Route::get("/hosts/{host}", [\App\Http\Controllers\Sig\SigHostController::class, 'update'])->name("hosts.update");
-    Route::resource("/hosts", \App\Http\Controllers\Sig\SigHostController::class);
+//    Route::get("/hosts", [SigHostController::class, 'index'])->name("hosts.index");
+//    Route::get("/hosts/{host}", [SigHostController::class, 'show'])->name("hosts.show");
+//    Route::get("/hosts/{host}/edit", [SigHostController::class, 'edit'])->name("hosts.edit");
+//    Route::get("/hosts/{host}", [SigHostController::class, 'update'])->name("hosts.update");
+    Route::resource("/hosts", SigHostController::class);
 
     // SIG Locations
-//    Route::get("/locations", [\App\Http\Controllers\Sig\SigLocationController::class, 'index'])->name("locations.index");
-//    Route::get("/locations/{location}", [\App\Http\Controllers\Sig\SigLocationController::class, 'show'])->name("locations.show");
-    Route::resource("/locations", \App\Http\Controllers\Sig\SigLocationController::class);
+//    Route::get("/locations", [SigLocationController::class, 'index'])->name("locations.index");
+//    Route::get("/locations/{location}", [SigLocationController::class, 'show'])->name("locations.show");
+    Route::resource("/locations", SigLocationController::class);
     // Timetable
-    Route::get("/timetable", [\App\Http\Controllers\TimetableController::class, 'index'])->name("timetable.index");
-    Route::post("/timetable", [\App\Http\Controllers\TimetableController::class, 'store'])->name("timetable.store");
-    Route::get("/timetable/{entry}/edit", [\App\Http\Controllers\TimetableController::class, "edit"])->name("timetable.edit");
-    Route::put("/timetable/{entry}", [\App\Http\Controllers\TimetableController::class, 'update'])->name("timetable.update");
-    Route::delete("/timetable/{entry}", [\App\Http\Controllers\TimetableController::class, 'destroy'])->name("timetable.destroy");
+    Route::get("/timetable", [TimetableController::class, 'index'])->name("timetable.index");
+    Route::post("/timetable", [TimetableController::class, 'store'])->name("timetable.store");
+    Route::get("/timetable/{entry}/edit", [TimetableController::class, "edit"])->name("timetable.edit");
+    Route::put("/timetable/{entry}", [TimetableController::class, 'update'])->name("timetable.update");
+    Route::delete("/timetable/{entry}", [TimetableController::class, 'destroy'])->name("timetable.destroy");
 });
 
