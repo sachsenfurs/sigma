66c593ed7c5c984917e604d9359081f9def47be5
Julian Wally <julian@wally.cloud>
2023-06-01 23:01:10 +0200

    [WIP][TASK] Update view for mobile devices

--- resources/views/public/timeslot-show.blade.php
+++ resources/views/public/timeslot-show.blade.php
@@ -3,6 +3,115 @@
 @section('content')
     <div class="container">
         <div class="card">
+            <div class="card-header text-center">
+                <strong>
+                    {{ $entry->sigEvent->name }}
+                    {{ $entry->sigEvent->name != $entry->sigEvent->name_en ? " | " . $entry->sigEvent->name_en : "" }}
+                </strong>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12 text-center col-md-12">
+                        <h2>Room</h2>
+                    </div>
+                    <div class="col-12 text-center col-md-12">
+                        <p><span class="badge bg-secondary">{{ $entry->sigEvent->sigLocation->name }}</span></p>
+                    </div>
+                </div>
+                <hr>
+                <div id="event-description">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-12 mb-4 mb-md-0 text-center">
+                            {!! nl2br($entry->sigEvent->description) !!}
+                        </div>
+                        <div class="col-lg-6 col-md-12 mb-4 mb-md-0 text-center">
+                            {!! nl2br($entry->sigEvent->description_en) !!}
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div>
+                    <div class="accordion" id="timeslot-list">
+                        @forelse($entry->sigEvent->timetableEntries AS $e)  
+                            @php
+                                $counter = 0;
+                            @endphp                               
+                            @if ($e->sigTimeslots->isEmpty() == false)
+                                @forelse ($e->sigTimeslots as $timeslot)
+                                    @if ($timeslot->max_users > $timeslot->sigAttendees->count())
+                                    @php
+                                        $counter = $counter + ($timeslot->max_users - $timeslot->sigAttendees->count());
+                                    @endphp
+                                    @endif
+                                @empty
+                                @endforelse
+                            @endif                                
+                            @if ($e->sigTimeslots->isEmpty() == false)
+                                <div class="accordion-item mt-1 mb-1">
+                                    <h2 class="accordion-header">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-ts_{{ $e->id }}" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                        {{ $e->start->format("d.m.Y") }} - {{ $e->start->format("H:i") }} - {{ $e->end->format("H:i") }}<br>{{ $counter }} Freie Plätze
+                                    </button>
+                                    </h2>
+                                    <div id="panelsStayOpen-collapse-ts_{{ $e->id }}" class="accordion-collapse collapse show">
+                                        @if ($counter != 0)
+                                            <div class="accordion-body">
+                                                @forelse ($e->sigTimeslots as $timeslot)
+                                                    @if ($timeslot->max_users > $timeslot->sigAttendees->count())
+                                                        <div class="row mb-3 mb-md-0">
+                                                            <div class="col-lg-2 col-6 border">
+                                                                <span class="align-middle text-center">{{ date('H:i', strtotime($timeslot->slot_start)) }} - {{ date('H:i', strtotime($timeslot->slot_end))  }}</span>
+                                                            </div>
+                                                            <div class="col-lg-2 col-6 border">
+                                                                <span>{{ $timeslot->sigAttendees->count() }}/{{$timeslot->max_users}}</span><br>
+                                                                <span>Slots taken</span>
+                                                            </div>
+                                                            @php
+                                                                $currentTime = Carbon\Carbon::now()->toDateTimeString();
+                                                                $regStart = strtotime($timeslot->reg_start);
+                                                                $regEnd = strtotime($timeslot->reg_end);
+                                                            @endphp
+                                                            @if ($currentTime > $regStart && $currentTime < $regEnd)
+                                                                <a class="col-lg-2 col-12 border align-middle text-center btn btn-primary" href="#">
+                                                                    Register
+                                                                </a>
+                                                            @elseif ($currentTime < $regStart)
+                                                                <a class="col-lg-2 col-12 border align-middle text-center btn btn-warning" disabled>
+                                                                    Registeration opens soon
+                                                                </a>
+                                                            @elseif ($currentTime > $regEnd)
+                                                                <a class="col-lg-2 col-12 border align-middle text-center btn btn-secondary" disabled>
+                                                                    Expired
+                                                                </a>
+                                                            @endif
+                                                        </div>
+                                                    @endif
+                                                @empty
+                                                    <p> All Slots are currently booked</p>
+                                                @endforelse
+                                            </div>
+                                        @else
+                                            <p>No more slots available.</p>
+                                        @endif
+                                        
+                                    </div>
+                                </div>  
+                            @else
+                                <p class="mt-1 mb-1">
+                                    <b>{{ $e->start->format("d.m.Y") }}</b><br>
+                                    {{ $e->start->format("H:i") }} - {{ $e->end->format("H:i") }}
+                                </p>
+                            @endif
+                        @empty
+                            Nicht im Programmplan
+                        @endforelse
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--
+        <h2>LEGACY</h2>
+        <div class="card">
             <div class="card-header">
                 {{ $entry->sigEvent->name }}
                 {{ $entry->sigEvent->name != $entry->sigEvent->name_en ? " | " . $entry->sigEvent->name_en : "" }}
@@ -13,38 +122,56 @@
                         <td>
                             <p>
                                 <span class="badge bg-secondary">{{ $entry->sigEvent->sigLocation->name }}</span>
-                                @if ($entry->sigEvent->SigTimeSlot->max_users >= $entry->sigEvent->SigTimeSlot->attendees->count() )
-                                    <a href="" class="btn btn-success" role="button">Register</a>
-                                @else
-                                    <a href="" class="btn btn-secondary disabled" role="button" aria-disabled="true">No more slots available</a>
-                                @endif
                             </p>
                         </td>
                         <td>
-                            <div class="accordion" id="accordionPanelsStayOpenExample">
-                                @forelse($entry->sigEvent->timetableEntries AS $e)
-                                    @if ($e->Timeslots->count() >= 1)
-                                        // Add Check for reg start & reg end
+                            <div class="accordion w-75 p-3" id="timeslot-list">
+                                @php
+                                    $counter = 0;
+                                @endphp
+                                @forelse($entry->sigEvent->timetableEntries AS $e)                                    
+                                    @if ($e->sigTimeslots->isEmpty() == false)
+                                        @forelse ($e->sigTimeslots as $timeslot)
+                                            @if ($timeslot->max_users > $timeslot->sigAttendees->count())
+                                            @php
+                                                $counter = $counter + ($timeslot->max_users - $timeslot->sigAttendees->count());
+                                            @endphp
+                                            @endif
+                                        @empty
+                                        @endforelse
+                                    @endif
+                                @empty
+                                @endforelse
+                                @forelse($entry->sigEvent->timetableEntries AS $e)                                    
+                                    @if ($e->sigTimeslots->isEmpty() == false)
                                         <div class="accordion-item">
                                             <h2 class="accordion-header">
-                                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                                <b>{{ $e->start->format("d.m.Y") }}</b><br>
-                                                {{ $e->start->format("H:i") }} - {{ $e->end->format("H:i") }}
+                                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-ts_{{ $e->id }}" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                                {{ $e->start->format("d.m.Y") }} - {{ $e->start->format("H:i") }} - {{ $e->end->format("H:i") }} | Freie Plätze: {{ $counter }}
                                             </button>
                                             </h2>
-                                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
-                                                <div class="accordion-body">
-                                                   @foreach ($e->Timeslots as $ts)
-                                                    <div class="card">
-                                                        <div class="card-body">
-                                                            {{ $ts->slot_start->format("H:i") }} - {{ $ts->slot_end->format("H:i") }}
-                                                            <button type="button" href="" class="btn btn-primary">Register</button>
-                                                        </div>
+                                            <div id="panelsStayOpen-collapse-ts_{{ $e->id }}" class="accordion-collapse collapse show">
+                                                @if ($counter != 0)
+                                                    <div class="accordion-body">
+                                                        @forelse ($e->sigTimeslots as $timeslot)
+                                                            @if ($timeslot->max_users > $timeslot->sigAttendees->count())
+                                                                <div class="card">
+                                                                    <div class="card-body">
+                                                                        {{ $timeslot->slot_start }} - {{ $timeslot->slot_end }} | {{ $timeslot->max_users - $timeslot->sigAttendees->count() }}/{{$timeslot->max_users}} Slots available
+                                                                        <button type="button" href="" class="btn btn-primary">Register</button>
+                                                                    </div>
+                                                                </div>
+                                                            @endif
+                                                        @empty
+                                                            <p> All Slots are currently booked</p>
+                                                        @endforelse
                                                     </div>
-                                                   @endforeach
-                                                </div>
+                                                @else
+                                                    <p>No more slots available.</p>
+                                                @endif
+                                                
                                             </div>
-                                        </div>
+                                        </div>  
                                     @else
                                         <p>
                                             <b>{{ $e->start->format("d.m.Y") }}</b><br>
@@ -65,5 +192,6 @@
                 </table>
             </div>
         </div>
+        -->
     </div>
 @endsection
