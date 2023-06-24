badefe234c6f5ecc445dd8ae8e2e3ab05c79b1ae
Julian Wally <julian@wally.cloud>
2023-06-24 23:55:37 +0200

    [WIP][TASK] Added dummy view examples for endusers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,196 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
+            <div class="row m-3">
+                <div class="col-12 col-md-12 text-center">
+                    <h2>Registrierungen</h2>
+                    <hr>
+                </div>
+                <div class="col-12 col-md-12 text-center">
+                    <p>Du hast aktuell noch keine Registrierungen</p>
+                    <div class="d-none d-xl-block">
+                        <a class="btn btn-primary" href="/table" role="button">Events entdecken</a>
+                    </div>
+                    <div class="d-block d-sm-none">
+                        <a class="btn btn-primary btn-lg" href="/table" role="button">Events entdecken</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row m-3">
+                <div class="col-12 col-md-12 text-center">
+                    <h2>Registrierungen</h2>
+                    <hr>
+                </div>
+                <div class="col-12 col-md-12 text-center">
+                    <div class="d-none d-xl-block">
+                        <div class="row border-bottom border-dark mb-2">
+                            <div class="col-3 col-md-3">
+                                <strong>Event</strong>
+                            </div>
+                            <div class="col-3 col-md-3">
+                                <strong>Datum</strong>
+                            </div>
+                            <div class="col-2 col-md-2">
+                                <strong>Zeitraum</strong>
+                            </div>
+                            <div class="col-2 col-md-2">
+                                <strong>Raum</strong>
+                            </div>
+                            <div class="col-2 col-md-2">
+                                <strong>Aktionen</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-12 col-md-3 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-middle">
+                                    <strong>Event</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    Fotoshooting
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-middle">
+                                    <strong>Datum</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    01.09.2023
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-2 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-middle">
+                                    <strong>Zeitraum</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    14:00 - 14:15
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-2 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-middle">
+                                    <strong>Raum</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    Suhl
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-2 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-items-center">
+                                    <strong>Aktionen</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    <div class="d-none d-xl-block">
+                                        <a type="button" class="btn btn-secondary text-white" onclick="$('#userModal').modal('show'); $('#deleteForm').attr('action', '/timeslots/--')" data-toggle="modal" data-target="#deleteModal" data-timeslot="--">
+                                            <span class="bi bi-people-fill"></span>
+                                        </a>
+                                        <button type="button" class="btn btn-danger text-white" onclick="$('#deleteModal').modal('show'); $('#deleteForm').attr('action', '/timeslots/--')" data-toggle="modal" data-target="#deleteModal" data-timeslot="--">
+                                            <span class="bi bi-x"></span>
+                                        </button>
+                                    </div>
+                                    <div class="d-block d-sm-none">
+                                        <a type="button" class="btn btn-secondary text-white btn-lg" onclick="$('#userModal').modal('show'); $('#deleteForm').attr('action', '/timeslots/--')" data-toggle="modal" data-target="#deleteModal" data-timeslot="--">
+                                            <span class="bi bi-people-fill"></span>
+                                        </a>
+                                        <button type="button" class="btn btn-danger text-white btn-lg" onclick="$('#deleteModal').modal('show'); $('#deleteForm').attr('action', '/timeslots/--')" data-toggle="modal" data-target="#deleteModal" data-timeslot="--">
+                                            <span class="bi bi-x"></span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-6 d-block d-sm-none mx-auto">
+                            <hr>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-12 col-md-3 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-middle">
+                                    <strong>Event</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    Volleyball
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-middle">
+                                    <strong>Datum</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    02.09.2023
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-2 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-middle">
+                                    <strong>Zeitraum</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    17:00 - 19:00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-2 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-middle">
+                                    <strong>Raum</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    Outdoor
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-2 mt-1 mb-1">
+                            <div class="row">
+                                <div class="col-6 col-md-6 d-block d-sm-none align-items-center">
+                                    <strong>Aktionen</strong>
+                                </div>
+                                <div class="col-6 col-md-12">
+                                    <div class="d-none d-xl-block">
+                                        <a type="button" class="btn btn-secondary text-white" onclick="$('#userModal').modal('show'); $('#deleteForm').attr('action', '/timeslots/--')" data-toggle="modal" data-target="#deleteModal" data-timeslot="--">
+                                            <span class="bi bi-people-fill"></span>
+                                        </a>
+                                        <button type="button" class="btn btn-danger text-white" onclick="$('#deleteModal').modal('show'); $('#deleteForm').attr('action', '/timeslots/--')" data-toggle="modal" data-target="#deleteModal" data-timeslot="--">
+                                            <span class="bi bi-x"></span>
+                                        </button>
+                                    </div>
+                                    <div class="d-block d-sm-none">
+                                        <a type="button" class="btn btn-secondary text-white btn-lg" onclick="$('#userModal').modal('show'); $('#deleteForm').attr('action', '/timeslots/--')" data-toggle="modal" data-target="#deleteModal" data-timeslot="--">
+                                            <span class="bi bi-people-fill"></span>
+                                        </a>
+                                        <button type="button" class="btn btn-danger text-white btn-lg" onclick="$('#deleteModal').modal('show'); $('#deleteForm').attr('action', '/timeslots/--')" data-toggle="modal" data-target="#deleteModal" data-timeslot="--">
+                                            <span class="bi bi-x"></span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-md-6 d-block d-sm-none mx-auto">
+                            <hr>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-12">
+                        <div class="d-none d-xl-block">
+                            <a class="btn btn-primary" href="/table" role="button">Weitere Events entdecken</a>
+                        </div>
+                        <div class="d-block d-sm-none">
+                            <a class="btn btn-primary btn-lg" href="/table" role="button">Weitere Events entdecken</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
