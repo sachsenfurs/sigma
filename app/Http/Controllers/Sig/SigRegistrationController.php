1165a3db6d9bbdba989154a29f73897c4e3c46af
Julian Wally <julian@wally.cloud>
2023-07-03 21:59:23 +0200

    [TASK] Update registration logic

--- app/Http/Controllers/Sig/SigRegistrationController.php
+++ app/Http/Controllers/Sig/SigRegistrationController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\SigTimeslot;
 use Redirect;
+use Illuminate\Support\Carbon;
 
 class SigRegistrationController extends Controller
 {
@@ -16,11 +17,17 @@ class SigRegistrationController extends Controller
     }
 
     public function register(SigTimeslot $timeslot) {
-        if($timeslot->max_users < $timeslot->sigAttendees->count()) {
+        $currentTime = strtotime(Carbon::now()->toDateTimeString());
+        $regStart = strtotime($timeslot->reg_start);
+        $regEnd = strtotime($timeslot->reg_end);
+        if($timeslot->max_users <= $timeslot->sigAttendees->count()) {
             return redirect()->back()->with('error', 'Dieser Timeslot ist bereits voll!');
-        } elseif(auth()->user()->attendeeEvents->has(auth()->user()->id)) {
-            /* TODO =>  Muss noch gefixt werden */
+        } elseif($timeslot->sigAttendees->contains('user_id', auth()->user()->id)) {
             return redirect()->back()->with('error', 'Du nimmst bereits an diesem Timeslot teil!');
+        } elseif($regStart > $currentTime) {
+            return redirect()->back()->with('error', 'Du kannst dich noch nicht registrieren!');
+        } elseif($regEnd < $currentTime) {
+            return redirect()->back()->with('error', 'Die Registrierung für dieses Event ist nicht mehr verfügbar!');
         } else {
             $timeslot->sigAttendees()->create(['user_id' => auth()->user()->id]);
             return redirect()->back()->with('success', 'Erfolreich für den Timeslot registriert!');
