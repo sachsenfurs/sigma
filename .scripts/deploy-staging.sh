7f8ea9c4a1dec6b9c803489fd7e71b83a695bb31
Stefan Großholz <mail@kidran.de>
2023-07-20 14:53:47 +0200

    Update deploy-staging.sh

--- .scripts/deploy-staging.sh
+++ .scripts/deploy-staging.sh
@@ -8,7 +8,7 @@ echo "Deployment started ..."
 (php artisan down) || true
 
 # Pull the latest version of the app
-git pull origin production
+git pull origin main
 
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
