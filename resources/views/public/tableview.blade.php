1940ee8d0b9ea7436dd8545e323fa11dbe88d048
kidran <mail@kidran.de>
2023-08-13 23:17:16 +0200

    Entwurf tableview

--- resources/views/public/tableview.blade.php
+++ resources/views/public/tableview.blade.php
@@ -156,6 +156,11 @@
                             <div class="card-body align-self-center text-center">
                                 <h2>{{ $entry->start->format("H:i") }}</h2>
                                 <h5 class="text-muted">{{ $entry->formatted_length }}</h5>
+                                @if($entry->cancelled)
+                                    <h3><span class="badge bg-danger d-block text-uppercase">{{ __("Cancelled") }}</span></h3>
+                                @elseif($entry->hasTimeChanged())
+                                    <h3><span class="badge bg-warning d-block text-uppercase">{{ __("Changed") }}</span></h3>
+                                @endif
                             </div>
                         </div>
                         <div class="col-lg-9 col-6 d-flex">
@@ -166,7 +171,13 @@
                                 </p>
                                 <p>
                                     <i class="bi bi-geo-alt"></i> {{ $entry->sigLocation->name }}
+                                    @if($entry->hasLocationChanged())
+                                        <span class="badge bg-danger">{{ __("Changed") }}</span>
+                                    @endif
                                 </p>
+                                @foreach($entry->sigEvent->sigTags AS $tag)
+                                    <h3 class="d-inline"><span class="badge bg-secondary">{{ $tag->description }}</span></h3>
+                                @endforeach
                             </div>
                         </div>
                         <div class="card-body col-lg-1 col-2 d-flex">
