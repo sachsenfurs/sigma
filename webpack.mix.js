d6e7c366f30a50680802af5cb42af2dfbd85bbed
kidran <mail@kidran.de>
2023-08-16 21:54:47 +0200

    fixed jquery

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-
     .sass('resources/sass/app.scss', 'public/css')
-    .vue()
     .sourceMaps()
+    .autoload({
+        jquery: ['$', 'jQuery', 'window.jQuery'],
+    })
+    .vue()
 ;
