3904a760061a49b240689dac1619d8b8187e6584
Kenthanar <d.ilday@kenthart.de>
2023-08-23 20:26:20 +0200

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,25 @@
-import "./bootstrap";
+import './bootstrap';
 
 // bootstrap
-import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-// jQuery
-import $ from "jquery";
+import $ from 'jquery';
 window.$ = window.jQuery = $;
-import "jquery-ui/ui/widgets/autocomplete";
-
+import 'jquery-ui/ui/widgets/autocomplete';
 // Vue
-import { createApp } from "vue";
-import ExampleComponent from "./components/ExampleComponent.vue";
-import EntryComponent from "./components/TimetableEntrys/EntryComponent.vue";
-import EntryList from "./components/TimetableEntrys/EntryList.vue";
-import DayTabComponent from "./components/TimetableEntrys/DayTabComponent.vue";
-import RoomTabComponent from "./components/TimetableEntrys/RoomTabComponent.vue";
-let app = createApp({
-    components: {
-        ExampleComponent,
-        EntryComponent,
-        EntryList,
-        DayTabComponent,
-        RoomTabComponent
-    },
-}).mount("#app");
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue'
+import TimetableEntryComponent from "./components/TimetableEntryComponent.vue";
+import TimetableEntryList from "./components/TimetableEntryList.vue";
+
+$(document).ready(function() {
+    if($('#app').length > 0) {
+        let app = createApp({
+            components: {
+                ExampleComponent,
+                TimetableEntryComponent,
+                TimetableEntryList
+            }
+        }).mount("#app");
+    }
+});
