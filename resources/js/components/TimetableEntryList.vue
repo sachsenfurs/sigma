0882648dd59719845030cbd9c303dd1d288688fa
Kenthanar <d.ilday@kenthart.de>
2023-08-23 20:23:57 +0200

    Bugfix TimeTable

---
+++ resources/js/components/TimetableEntryList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <timetable-entry-component v-for="(entry, id) in entries" :entry="entry" :key="entry.id"></timetable-entry-component>
+    </div>
+
+
+</template>
+
+<script>
+import timetableEntryComponent from "./TimetableEntryComponent.vue";
+export default  {
+    components: {
+        timetableEntryComponent
+    },
+    methods: {
+        async getEntries() {
+            let events = await axios.get("/table/index") ;
+            // console.log(events.data);
+            this.entries = events.data;
+        }
+    },
+    data() {
+        return {
+            entries: this.getEntries(),
+        }
+    }
+
+}
+</script>
+<style scoped>
+
+</style>
