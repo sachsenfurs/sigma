a4cc1c4d76c47d987a8d3bc95a134dcbb688c593
Julian Wally <julian@wally.cloud>
2023-08-30 20:42:24 +0200

    [TASK] Make it prettier

--- resources/views/components/list-attendee-item.blade.php
+++ resources/views/components/list-attendee-item.blade.php
@@ -8,7 +8,11 @@
             <img src="{{ $avatar }}" class="img-fluid h-100 w-100 rounded-top" style="object-fit: cover; max-height: 30vw" alt="">
         </div>
     @endif
-    <div class="@if($avatar)col-md-10 @else col-md-12 @endif">
-        {{ $name }}
+    <div class="@if($avatar)col-md-10 @else col-md-12 text-left @endif">
+        @if ($avatar)
+            {{ $name }}
+        @else
+            <li>{{ $name }}</li>
+        @endif
     </div>
 </div>
