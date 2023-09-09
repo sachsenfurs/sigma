5a8798b407738799103b686c2c9a277179620909
Julian Wally <julian@wally.cloud>
2023-09-09 22:17:33 +0200

    [TASK] Make buttons better :)

--- resources/views/components/timeslot/button.blade.php
+++ resources/views/components/timeslot/button.blade.php
@@ -2,6 +2,9 @@
     'disabled' => true,
     'class' => "btn-primary",
 ])
-<a class="col-lg-4 col-12 border align-middle text-center btn {{ $class }}" @if($disabled) disabled @endif {{ $attributes->only("onclick") }}>
-    {{ $slot }}
-</a>
+<div class="col-lg-4 col-12 align-self-center p-1 my-1">
+    <a class="align-middle text-center btn w-100 {{ $class }}" @if($disabled) disabled @endif {{ $attributes->only("onclick") }} style="height: 40px;">
+        {{ $slot }}
+    </a>
+</div>
+
