8377f8ce0c62872e6f41b8b286554308ee23036d
kidran <mail@kidran.de>
2023-09-10 09:00:11 +0200

    removed fixed height

--- resources/views/components/timeslot/button.blade.php
+++ resources/views/components/timeslot/button.blade.php
@@ -3,7 +3,7 @@
     'class' => "btn-primary",
 ])
 <div class="col-lg-4 col-12 align-self-center p-1 my-1">
-    <a class="align-middle text-center btn w-100 {{ $class }}" @if($disabled) disabled @endif {{ $attributes->only("onclick") }} style="height: 40px;">
+    <a class="align-middle text-center btn w-100 {{ $class }}" @if($disabled) disabled @endif {{ $attributes->only("onclick") }}>
         {{ $slot }}
     </a>
 </div>
