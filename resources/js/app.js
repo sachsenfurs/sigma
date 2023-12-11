b57478a27f2e538567ca2695e3abffb55957be45
kidran <mail@kidran.de>
2023-12-12 00:29:40 +0100

    updated to laravel 10, removed mix, added vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,10 @@ import './bootstrap';
 // bootstrap
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import $ from 'jquery';
-window.$ = window.jQuery = $;
-import 'jquery-ui/ui/widgets/autocomplete';
 
 // Vue
-import { createApp } from 'vue';
+import { createApp } from 'vue/dist/vue.esm-bundler'
+
 import RoomTabComponent from "./components/TimetableEntries/RoomTabComponent.vue";
 import EntryComponent from "./components/TimetableEntries/EntryComponent.vue";
 import EntryList from "./components/TimetableEntries/EntryList.vue";
@@ -16,19 +14,15 @@ import DayTabComponent from "./components/TimetableEntries/DayTabComponent.vue";
 
 import { i18nVue } from 'laravel-vue-i18n'
 
-$(document).ready(function() {
-    if($('#app').length > 0) {
-        let app = createApp({
-            components: {
-                EntryComponent,
-                EntryList,
-                DayTabComponent,
-                RoomTabComponent,
-            }
-        })
-        .use(i18nVue, {
-            resolve: (lang) => import(`../../lang/${lang}.json`)
-        })
-        .mount("#app");
+let app = createApp({
+    components: {
+        EntryComponent,
+        EntryList,
+        DayTabComponent,
+        RoomTabComponent,
     }
-});
+})
+.use(i18nVue, {
+    resolve: (lang) => import(`../../lang/${lang}.json`)
+})
+.mount("#app");
