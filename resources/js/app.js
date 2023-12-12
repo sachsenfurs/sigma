5a75836315953948a0e8ea45f28689afc752a7e2
Adrian Jakob <d3xter@d3xter.dev>
2023-12-12 13:04:33 +0100

    removed runtime compiler issues

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,23 +5,17 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 // Vue
-import RoomTabComponent from "./components/TimetableEntries/RoomTabComponent.vue";
-import EntryComponent from "./components/TimetableEntries/EntryComponent.vue";
 import EntryList from "./components/TimetableEntries/EntryList.vue";
-import DayTabComponent from "./components/TimetableEntries/DayTabComponent.vue";
 
-import { i18nVue } from 'laravel-vue-i18n'
+import {i18nVue} from 'laravel-vue-i18n'
 import {createApp} from "vue";
 
-createApp({
-    components: {
-        EntryComponent,
-        EntryList,
-        DayTabComponent,
-        RoomTabComponent,
-    }
-})
-.use(i18nVue, {
-    resolve: (lang) => import(`../../lang/${lang}.json`)
-})
-.mount("#app");
+const addVueApp = (id, component) => {
+    const app = createApp(component);
+    app.use(i18nVue, {
+        resolve: (lang) => import(`../../lang/${lang}.json`)
+    })
+    app.mount(`#${id}`);
+}
+
+addVueApp("app", EntryList);
