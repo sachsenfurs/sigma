eca3a7e09d1c8254720f0a49b1657574bc23d40d
Kidran <mail@kidran.de>
2024-05-13 14:49:05 +0200

    added new SigLocation fields to filament

--- app/Filament/Resources/SigLocationResource.php
+++ app/Filament/Resources/SigLocationResource.php
@@ -21,38 +21,55 @@ class SigLocationResource extends Resource
     protected static ?string $navigationGroup = 'SIG';
     protected static ?int $navigationSort = 100;
 
-    public static function can(string $action, ?Model $record = null): bool
-    {
+    public static function can(string $action, ?Model $record = null): bool {
         return auth()->user()->permissions()->contains('manage_sig_base_data');
     }
 
-    public static function getLabel(): ?string
-    {
+    public static function getLabel(): ?string {
         return __('Location');
     }
 
-    public static function getPluralLabel(): ?string
-    {
+    public static function getPluralLabel(): ?string {
         return __('Locations');
     }
 
-    public static function form(Form $form): Form
-    {
+    public static function form(Form $form): Form {
         return $form
             ->schema([
                 self::getNameField(),
+                self::getNameEnField(),
                 self::getDescriptionField(),
-                self::getRenderIdField(),
-                self::getFloorField(),
-                self::getRoomField(),
-                self::getInfodisplayField(),
-                self::getRoomSizeField(),
-                self::getSeatsField(),
+                self::getDescriptionEnField(),
+
+                Forms\Components\Fieldset::make()
+                    ->label("Physical Information")
+                    ->translateLabel()
+                    ->columns(4)
+                    ->schema([
+                        self::getFloorField(),
+                        self::getRoomField(),
+                        self::getRoomSizeField(),
+                        self::getSeatsField(),
+                    ]),
+                Forms\Components\Fieldset::make()
+                    ->columns(4)
+                    ->label("Signage Info")
+                    ->translateLabel()
+                    ->schema([
+                        self::getRenderIdField()->columnSpan(2),
+                        self::getInfodisplayField(),
+                        self::getEssentialField(),
+                        Forms\Components\Grid::make()
+                             ->schema([
+                                 self::getEssentialDescriptionField(),
+                                 self::getEssentialDescriptionEnField(),
+                             ])
+                    ]),
+
             ]);
     }
 
-    public static function table(Table $table): Table
-    {
+    public static function table(Table $table): Table {
         return $table
             ->columns(self::getTableColumns())
             ->filters([
@@ -63,15 +80,13 @@ class SigLocationResource extends Resource
             ]);
     }
 
-    public static function getRelations(): array
-    {
+    public static function getRelations(): array {
         return [
             RelationManagers\SigEventsRelationManager::class,
         ];
     }
 
-    public static function getPages(): array
-    {
+    public static function getPages(): array {
         return [
             'index' => Pages\ListSigLocations::route('/'),
             'create' => Pages\CreateSigLocation::route('/create'),
@@ -79,100 +94,136 @@ class SigLocationResource extends Resource
         ];
     }
 
-    private static function getTableColumns(): array
-    {
+    private static function getTableColumns(): array {
         return [
             Tables\Columns\TextColumn::make('name')
-                ->label('Name')
-                ->translateLabel()
-                ->searchable()
-                ->sortable(),
+                                     ->label('Name')
+                                     ->translateLabel()
+                                     ->searchable()
+                                     ->sortable(),
             Tables\Columns\TextColumn::make('description')
-                ->label('Description')
-                ->translateLabel()
-                ->searchable(),
+                                     ->label('Description')
+                                     ->translateLabel()
+                                     ->searchable(),
             Tables\Columns\TextColumn::make('floor')
-                ->label('Floor')
-                ->translateLabel()
-                ->searchable()
-                ->sortable(),
+                                     ->label('Floor')
+                                     ->translateLabel()
+                                     ->searchable()
+                                     ->sortable(),
             Tables\Columns\TextColumn::make('room')
-                ->label('Room')
-                ->translateLabel()
-                ->searchable()
-                ->sortable(),
+                                     ->label('Room')
+                                     ->translateLabel()
+                                     ->searchable()
+                                     ->sortable(),
             Tables\Columns\TextColumn::make('sig_events_count')
-                ->label('Event count')
-                ->translateLabel()
-                ->counts('sigEvents')
-                ->sortable(),
+                                     ->label('Event count')
+                                     ->translateLabel()
+                                     ->counts('sigEvents')
+                                     ->sortable(),
             Tables\Columns\IconColumn::make('infodisplay')
-                ->label('Infodisplay')
-                ->translateLabel()
-                ->boolean()
-                ->sortable(),
+                                     ->label('Infodisplay')
+                                     ->translateLabel()
+                                     ->boolean()
+                                     ->sortable(),
         ];
     }
 
-    private static function getNameField(): Forms\Components\Component
-    {
+    private static function getNameField(): Forms\Components\Component {
         return Forms\Components\TextInput::make('name')
-            ->label('Name')
-            ->translateLabel()
-            ->required()
-            ->maxLength(255);
+                                         ->label('Name')
+                                         ->translateLabel()
+                                         ->required()
+                                         ->maxLength(255);
     }
 
-    private static function getDescriptionField(): Forms\Components\Component
-    {
+    private static function getNameEnField(): Forms\Components\Component {
+        return Forms\Components\TextInput::make('name_en')
+                                         ->label('Name (English) (optional, defaults to Name)')
+                                         ->translateLabel()
+                                         ->nullable()
+                                         ->maxLength(255);
+    }
+
+    private static function getDescriptionField(): Forms\Components\Component {
         return Forms\Components\TextInput::make('description')
-            ->label('Description')
-            ->translateLabel()
-            ->maxLength(255);
+                                         ->label('Functional Description ("Main Stage", ..)')
+                                         ->nullable()
+                                         ->translateLabel()
+                                         ->maxLength(255);
     }
 
-    private static function getRenderIdField(): Forms\Components\Component
-    {
+    private static function getDescriptionEnField(): Forms\Components\Component {
+        return Forms\Components\TextInput::make('description_en')
+                                         ->label('Functional Description (English)')
+                                         ->nullable()
+                                         ->translateLabel()
+                                         ->maxLength(255);
+    }
+
+    private static function getRenderIdField(): Forms\Components\Component {
         return Forms\Components\TextInput::make('render_ids');
     }
 
-    private static function getFloorField(): Forms\Components\Component
-    {
+    private static function getFloorField(): Forms\Components\Component {
         return Forms\Components\TextInput::make('floor')
-            ->label('Floor')
-            ->translateLabel()
-            ->maxLength(255);
+                                         ->label('Floor')
+                                         ->translateLabel()
+                                         ->nullable()
+                                         ->maxLength(255);
     }
 
-    private static function getRoomField(): Forms\Components\Component
-    {
+    private static function getRoomField(): Forms\Components\Component {
         return Forms\Components\TextInput::make('room')
-            ->label('Room')
-            ->translateLabel()
-            ->maxLength(255);
+                                         ->label('Room')
+                                         ->translateLabel()
+                                         ->nullable()
+                                         ->maxLength(255);
     }
 
-    private static function getInfodisplayField(): Forms\Components\Component
-    {
+    private static function getInfodisplayField(): Forms\Components\Component {
         return Forms\Components\Toggle::make('infodisplay')
-            ->label('Digital display in front of the door?')
-            ->translateLabel()
-            ->inline(false);
+                                      ->label('Digital display in front of the door?')
+                                      ->translateLabel()
+                                      ->inline(false);
     }
 
-    private static function getRoomSizeField(): Forms\Components\Component
-    {
+    private static function getRoomSizeField(): Forms\Components\Component {
         return Forms\Components\TextInput::make('roomsize')
-            ->label('Room Size')
-            ->translateLabel()
-            ->maxLength(255);
+                                         ->label('Room Size')
+                                         ->translateLabel()
+                                         ->nullable()
+                                         ->maxLength(255);
     }
 
-    private static function getSeatsField(): Forms\Components\Component
-    {
+    private static function getSeatsField(): Forms\Components\Component {
         return Forms\Components\TextInput::make('seats')
-            ->label('Seats')
-            ->translateLabel()
-            ->maxLength(255);
+                                         ->label('Seats')
+                                         ->translateLabel()
+                                         ->nullable()
+                                         ->maxLength(255);
+    }
+
+    private static function getEssentialDescriptionField(): Forms\Components\Component {
+        return Forms\Components\Textarea::make('essential_description')
+                                        ->label('Essential Description')
+                                        ->translateLabel()
+                                        ->nullable()
+                                        ->rows(5);
+    }
+
+    private static function getEssentialDescriptionEnField(): Forms\Components\Component {
+        return Forms\Components\Textarea::make('essential_description_en')
+                                        ->label('Essential Description (English)')
+                                        ->translateLabel()
+                                        ->nullable()
+                                        ->rows(5);
+    }
+
+    private static function getEssentialField(): Forms\Components\Component {
+        return Forms\Components\Toggle::make("essential")
+                                      ->label("Show in Essentials (Signage)")
+                                      ->translateLabel()
+                                      ->inline(false)
+                                      ->default(false);
     }
 }
