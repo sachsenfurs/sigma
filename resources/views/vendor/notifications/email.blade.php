d5bd53ff8a33043dfac5a4f34bf3ea1b87c02024
Julian Wally <julian@wally.cloud>
2024-06-23 19:45:21 +0200

    [TASK] Remove greeting because it is currently not needed

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,15 +1,4 @@
 <x-mail::message>
-{{-- Greeting --}}
-@if (! empty($greeting))
-# {{ $greeting }}
-@else
-@if ($level === 'error')
-# @lang('Whoops!')
-@else
-# @lang('Hello!')
-@endif
-@endif
-
 {{-- Intro Lines --}}
 @foreach ($introLines as $line)
 {{ $line }}
