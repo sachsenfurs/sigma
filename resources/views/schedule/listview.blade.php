bf0ff6451265c5da5f47c0c96676043592533ad5
Kidran <mail@kidran.de>
2024-06-27 00:52:02 +0200

    fixed policy

--- resources/views/schedule/listview.blade.php
+++ resources/views/schedule/listview.blade.php
@@ -2,7 +2,7 @@
 @section('title', __("Event Schedule"))
 
 @section('content')
-    @if(app(\App\Settings\AppSettings::class)->show_schedule_date->isAfter(now()) AND auth()->user()?->cannot("viewAny", \App\Models\TimetableEntry::class))
+    @if(app(\App\Settings\AppSettings::class)->show_schedule_date->isAfter(now()) AND \Illuminate\Support\Facades\Gate::denies("viewAny", \App\Models\TimetableEntry::class))
         <x-infocard>
             {{ __("The Schedule is not published yet") }}
         </x-infocard>
