aec122f50c841dfc7fb4613407608e81f76868d8
Julian Wally <julian@wally.cloud>
2024-06-29 16:35:43 +0200

    [TASK] Make form prettier

--- resources/views/filament/resources/chats/pages/view-chat.blade.php
+++ resources/views/filament/resources/chats/pages/view-chat.blade.php
@@ -43,18 +43,17 @@
                     </div>
                 @endforelse
             </div>
-            <div className="border">
+            <div class="border">
                 <form method="POST" action="{{ route('chats.store', $record->id) }}">
                     @csrf
-                    <div class="grid grid-rows-1 border-top mt-3 p-2">
-                        <div class="columns-12 columns-10md">
-                            <textarea name="text" type="text" class="form-control"></textarea>
-                        </div>
-                        <div class="columns-12 columns-2md p-2">
-                            <div class="d-flex justify-content-center">
-                                <button class="" type="submit">{{ __("Send")}}</button>
-                            </div>
-                        </div>
+                    <div class="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
+                        <textarea name="text" rows="1" class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+                        <button type="submit" class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+                            <svg class="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z"/>
+                            </svg>
+                            <span class="sr-only">Send message</span>
+                        </button>
                     </div>
                 </form>
             </div>
