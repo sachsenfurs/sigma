470aaec14a8d2046c179c0131da59e44aa4906e8
Kidran <mail@kidran.de>
2024-07-19 00:11:40 +0200

    ordered langauge flags

--- resources/views/filament/tables/columns/sig-event/flag-icon.blade.php
+++ resources/views/filament/tables/columns/sig-event/flag-icon.blade.php
@@ -1,4 +1,4 @@
-@foreach($getRecord()->languages ?? $getRecord()->sigEvent->languages ?? [] as $language)
+@foreach(collect($getRecord()->languages)->sort() ?? collect($getRecord()->sigEvent->languages)->sort() ?? [] as $language)
     <img src="/icons/{{ $language }}-flag.svg" alt="{{ $language }}" style="height: 1em; margin-right: 5px">
 @endforeach
 
