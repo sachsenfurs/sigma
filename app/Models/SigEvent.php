5d45cf2adeb2ca86e078e500e395727b4716825a
Kidran <mail@kidran.de>
2024-08-04 14:02:59 +0200

    removed unused import

--- app/Models/SigEvent.php
+++ app/Models/SigEvent.php
@@ -13,7 +13,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 
 #[ObservedBy(SigEventObserver::class)]
