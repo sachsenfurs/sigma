b661975dc4b25cd3447c95227f83c1909e660c52
Kidran <mail@kidran.de>
2024-08-05 13:43:54 +0200

    added dealers deadline info

--- app/Http/Controllers/Ddas/DealersDenController.php
+++ app/Http/Controllers/Ddas/DealersDenController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Ddas;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ddas\Dealer;
+use App\Policies\Ddas\DealerPolicy;
+use App\Settings\DealerSettings;
 
 class DealersDenController extends Controller
 {
@@ -14,6 +16,8 @@ class DealersDenController extends Controller
     }
 
     public function create() {
+        if(!DealerPolicy::isWithinDeadline())
+            return redirect()->route("home")->withError(__("The deadline for dealers application has already passed"));
         return view("ddas.dealers.create");
     }
 }
