0c3ce0714e34c22871dbc37e0f0aa4b965d2cf7e
Kidran <mail@kidran.de>
2024-08-09 21:54:20 +0200

    added artshow register button

--- resources/views/ddas/artshow/index.blade.php
+++ resources/views/ddas/artshow/index.blade.php
@@ -3,6 +3,24 @@
 
 @section('content')
     <div class="container">
+        <div class="card my-3 bg-info-subtle">
+            @can("create", \App\Models\Ddas\ArtshowItem::class)
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-6">
+                            {{ __("Would you like to register something for the art show?") }}
+                        </div>
+                        <div class="col-6 text-end">
+                            <a href="{{ route("artshow.create") }}">
+                                <button class="btn btn-primary">
+                                    {{ __("Click here to register") }}
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            @endcan
+        </div>
         @if(app(\App\Settings\ArtShowSettings::class)->show_items_date->isAfter(now()))
             <x-infocard>
                 {{ __("Art Show Items are not published yet") }}
