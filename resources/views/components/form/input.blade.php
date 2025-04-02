bc7c980cd7815eae2dc1d818132c079a8e0f97e6
Kidran <mail@kidran.de>
2025-04-02 21:09:33 +0200

    fixed: don't submit modal when pressing enter in a textarea

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -4,7 +4,7 @@
     'id' => null,
 ])
 @if($type == "textarea")
-<textarea name="{{$name}}" id="{{ $id ?? $name }}" {{ $attributes->merge(['class' => "form-control"])->except("value") }}>{{ $attributes->get("value", old($name)) }}</textarea>
+<textarea name="{{$name}}" id="{{ $id ?? $name }}" {{ $attributes->merge(['class' => "form-control"])->except("value") }} wire:keydown.stop.enter>{{ $attributes->get("value", old($name)) }}</textarea>
 @else
 <input type="{{ $type }}" name="{{$name}}" id="{{ $id ?? $name }}" {{ $attributes->merge(['class' => "form-control"]) }} value="{{ old($name) }}">
 @endif
