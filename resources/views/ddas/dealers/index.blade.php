06337a3a1c251cb3727f56c273c89d6c36bee587
Kidran <mail@kidran.de>
2025-04-14 22:11:12 +0200

    fixed layout

--- resources/views/ddas/dealers/index.blade.php
+++ resources/views/ddas/dealers/index.blade.php
@@ -65,11 +65,11 @@
                                             </a>
                                         </div>
                                     </div>
-                                    <div class="col-auto text-center">
+                                    <div class="col-auto text-end">
                                         <div x-show="dealer.icon_file" class="align-self-start justify-content-end p-3">
                                             <img x-bind:src="dealer.icon_file" class="img-fluid" style="max-height: 10em" alt="">
                                         </div>
-                                        <div class="mx-1 p-4 d-block">
+                                        <div class="mx-1 p-4 d-block text-center">
                                             <i x-show="dealer.location" class="bi bi-geo-alt icon-link"></i>
                                             <span x-text="dealer.location"></span>
                                         </div>
