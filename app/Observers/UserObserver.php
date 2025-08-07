87b26ad4c5b90d72104cf5b478a9b60bfc6248b7
Kidran <mail@kidran.de>
2025-08-07 23:33:21 +0200

    fixed group assignment

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -8,35 +8,32 @@ use Illuminate\Support\Str;
 
 class UserObserver
 {
-    public function created(User $user) {
+    public function created(User $user): void {
         $this->createUpdate($user);
     }
 
-    public function updated(User $user) {
+    public function updated(User $user): void {
         $this->createUpdate($user);
     }
 
-    private function createUpdate(User $user) {
+    private function createUpdate(User $user): void {
         $userRoles = UserRole::all()->pluck('registration_system_key', 'id')->map(function($item) {
             return $item != null ? explode(',', Str::lower($item)) : [];
         });
 
         $userGroups = $user->groups;
-        if (!$userGroups) {
+        if (!$userGroups)
             return;
-        }
+
         foreach ($userGroups as $userGroup) {
-            if (!$userGroup) {
+            if (!$userGroup)
                 continue;
-            }
-            $userRole = $userRoles->search(function ($item) use ($userGroup) {
+
+            $appliedUserRoles = $userRoles->where(function ($item) use ($userGroup) {
                 return in_array($userGroup, $item);
             });
 
-            if ($userRole AND !$user->roles->contains($userRole)) {
-                $user->roles()->attach($userRole);
-                $user->saveQuietly(); // save() würde in nem endlos loop enden... hab ich gehört... >.>
-            }
+            $user->roles()->syncWithoutDetaching($appliedUserRoles->keys());
         }
     }
 
