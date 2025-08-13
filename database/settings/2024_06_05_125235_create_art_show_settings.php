c4b17175ba9b94ce2ee25c819a1b683c9f88271b
Kidran <mail@kidran.de>
2025-08-14 00:04:35 +0200

    small fixes

--- database/settings/2024_06_05_125235_create_art_show_settings.php
+++ database/settings/2024_06_05_125235_create_art_show_settings.php
@@ -7,10 +7,9 @@ return new class extends SettingsMigration
 {
     public function up(): void {
         $this->migrator->inGroup('artshow', function (SettingsBlueprint $blueprint): void {
-            $blueprint->add('enabled', env("ARTSHOW.ENABLED", true));
-            $blueprint->add('item_deadline', env("ARTSHOW.ITEM_DEADLINE", now()->addDays(30)->setHour(0)->setMinute(0)->setSecond(0)));
-            $blueprint->add('show_items_date', env("ARTSHOW.SHOW_ITEMS_DATE", now()->addDays(30)->setHour(0)->setMinute(0)->setSecond(0)));
-
+            $blueprint->add('enabled', config("app.artshow.enabled"));
+            $blueprint->add('item_deadline', config("app.artshow.item_deadline"));
+            $blueprint->add('show_items_date', config("app.artshow.show_items_date"));
         });
     }
 };
