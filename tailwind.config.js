8d6d70593434525a8e77c2087b2cee3d7cff448c
Kidran <mail@kidran.de>
2026-06-24 12:45:14 +0200

    tailwind version fix

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+    content: [
+        './app/**/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './vendor/filament/**/*.blade.php',
+        './vendor/guava/calendar/resources/**/*.blade.php',
+    ],
+}
